dab4288b55b47fc27dba78c2092c80b475e76eda
Ankitbisht28 <127379059+Ankitbisht28@users.noreply.github.com>
2023-07-28 16:09:05 +0530

    Create nft's.js

---
+++ metadata/nft's.js
@@ -0,0 +1,32 @@
+const nfts = [
+    [
+        {
+          "name": "1",
+          "description": "Animal art 1",
+          "image": "https://gateway.pinata.cloud/ipfs/QmXD2pfNkdYEsCSHB53wgrgEfbcWktXi21zz913imPstqm/0.png"
+        },
+        {
+          "name": "2",
+          "description": "Animal art 2",
+          "image": "https://gateway.pinata.cloud/ipfs/QmXD2pfNkdYEsCSHB53wgrgEfbcWktXi21zz913imPstqm/1.png"
+        },
+        {
+          "name": "3",
+          "description": "Animal art 3",
+          "image": "https://gateway.pinata.cloud/ipfs/QmXD2pfNkdYEsCSHB53wgrgEfbcWktXi21zz913imPstqm/2.png"
+        },
+        {
+          "name": "4",
+          "description": "Animal art 4",
+          "image": "https://gateway.pinata.cloud/ipfs/QmXD2pfNkdYEsCSHB53wgrgEfbcWktXi21zz913imPstqm/3.png"
+        },
+        {
+          "name": "5",
+          "description": "Animal art 5",
+          "image": "https://gateway.pinata.cloud/ipfs/QmXD2pfNkdYEsCSHB53wgrgEfbcWktXi21zz913imPstqm/4.png"
+        }
+      ]
+      
+];
+
+module.exports = nfts;
